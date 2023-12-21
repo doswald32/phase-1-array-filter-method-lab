253bad0b02fbec6acc2d4bdcc85ec4958e36a7dc
doswald32 <dan.oswald32@gmail.com>
2023-12-20 19:14:02 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(array, name) {
+    const match = array.filter((driver) => driver.toLowerCase() === name.toLowerCase());
+    return match;
+}
+
+function fuzzyMatch(array, name) {
+    const match = array.filter((driver) => driver[0] === name[0]);
+    return match;
+}
+
+function matchName(array, name) {
+    const match = array.filter((driver) => driver.name === name);
+    return match;
+}
